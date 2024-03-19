340d2e6fbc703d5d6e791001c4b64ee12f6162e0
MatheuAlberto <mathhenriq15.09.03@gmail.com>
2024-03-19 18:59:47 -0400

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleMode(){
+    const html = document.documentElement
+
+/* Forma de como a máquina iria ler o código.
+
+    if(html.classList.contains('light')){
+       html.classList.remove('light') 
+    } else{
+        html.classList.add('light')
+    }
+
+*/
+    html.classList.toggle('light') //já coloca e retira automaticamente o ,light.
+
+    /* usaremos o mesmo comando if/else para substituir imagem*/ 
+    const img = document.querySelector("#profile img")
+    if(html.classList.contains('light')){
+        // se tiver light mode, adiciona a img light
+        img.setAttribute('src', './Stylss/avatar-light.png')
+        img.setAttribute('alt', 'imagem do Matheus, olhando para o lado esquerdo, usando um cabelo de samurai e imagem preto e branco')
+     } else{
+        // se não tiver light mode, adiciona a img light
+        img.setAttribute('src', './Stylss/Avatar.png')
+     }
+
+}
